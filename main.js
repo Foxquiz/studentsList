ed103a388431c41a0631d85e2a99e084986d2292
Arina Bardyukova <arina.bardyukova@yandex.ru>
2023-12-05 00:18:17 +0300

    adds filter, sorting, small fixes

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const studentsList = [
     id: 1,
     name: 'Илья',
     surname: 'Гусев',
-    patronymic: 'Анатольевич',
+    patronymic: 'Сергеевич',
     birthDate: '2004-11-30',
     startEducationYear: 2020,
-    faculty: 'Биологический'
+    faculty: 'Информатика и системы управления'
   },
   {
     id: 2,
@@ -47,9 +47,26 @@ const studentsList = [
     birthDate: '2002-04-11',
     startEducationYear: 2019,
     faculty: 'Машиностроительные технологии'
-  }
+  },
+  {
+    id: 6,
+    name: 'Виктор',
+    surname: 'Баринов',
+    patronymic: 'Алексеевич',
+    birthDate: '1998-02-10',
+    startEducationYear: 2016,
+    faculty: 'Лингвистический'
+  },
+  {
+    id: 7,
+    name: 'Марк',
+    surname: 'Баринов',
+    patronymic: 'Алексеевич',
+    birthDate: '2001-12-05',
+    startEducationYear: 2018,
+    faculty: 'Лингвистический'
+  },
 ]
-
 
 // Этап 3. Создайте функцию вывода одного студента в таблицу, по аналогии с тем, как вы делали вывод одного дела в модуле 8. Функция должна вернуть html элемент с информацией и пользователе.У функции должен быть один аргумент - объект студента.
 const currentDate = new Date();
@@ -62,9 +79,7 @@ let studentsTable = document.querySelector('#studentsTable');
 function ageWord(age) {
   let stringAge = age.toString();
   let secondDigit = stringAge[1];
-
   let ageWord = '';
-
   switch (secondDigit) {
     case '1':
       ageWord = 'год'
@@ -102,8 +117,7 @@ function getStudentItem(studentObj) {
   cellFaculty.textContent = studentObj.faculty;
 
   //считаем текущий возраст
-  //!нужно при расчёте принимать значение времени 00:00, а то др день в день считает некорректно
-
+  //!вопросы к корректности расчёта возраста когда датарождения=датаТекущая до 3-4 ночи
   let birthDate = new Date(studentObj.birthDate);
   let hoursBirthDate = birthDate.getHours() * (1000 * 60 * 60);
   let localFormatBirthDate = ('0' + birthDate.getDate()).slice(-2) + '.' + ('0' + (birthDate.getMonth() + 1)).slice(-2) + '.' + birthDate.getFullYear();
@@ -111,11 +125,14 @@ function getStudentItem(studentObj) {
   cellAge.textContent = localFormatBirthDate + ` (${studentsAge}` + ' ' + `${ageWord(studentsAge)})`;
 
   console.log('birthDate', birthDate);
+  console.log('hoursBirthDate', hoursBirthDate);
+  console.log('currentDate', currentDate);
 
   //считаем годы обучения
   let startEducationDate = studentObj.startEducationYear;
   let endEducationYear = startEducationDate + STUDY_PERIOD;
   let studyCourse = currentYear - startEducationDate;
+
   if (currentYear > endEducationYear || (currentYear === endEducationYear && currentMonth >= 8)) cellStudyPeriod.textContent = startEducationDate + `-` + endEducationYear + ' (закончил)';
 
   if (currentYear < endEducationYear || (currentYear === endEducationYear && currentMonth < 8))
@@ -125,8 +142,15 @@ function getStudentItem(studentObj) {
 }
 
 // Этап 4. Создайте функцию отрисовки всех студентов. Аргументом функции будет массив студентов.Функция должна использовать ранее созданную функцию создания одной записи для студента.Цикл поможет вам создать список студентов.Каждый раз при изменении списка студента вы будете вызывать эту функцию для отрисовки таблицы.
+function clearTable() {
+  if (!studentsTable) {
+    return;
+  }
+  studentsTable.replaceChildren();
+}
 
 function renderStudentsTable(studentsArray) {
+  clearTable();
   studentsArray.forEach(element => {
     getStudentItem(element)
   });
@@ -154,6 +178,44 @@ function getInputValue(inputElement) {
 function clearInputValue(inputElement) {
   inputElement.value = '';
 }
+
+//функции валидации input
+function validateText(text) {
+  let checkedText = false;
+  const regexp = /[а-яА-ЯёЁ]/i;
+  if (text.length > 0 && regexp.test(text)) {
+    checkedText = text[0].toUpperCase() + text.slice(1).toLowerCase();
+  };
+  return checkedText;
+};
+
+function validateDate(date) {
+  const birthDate = new Date(date);
+  const startDate = new Date('1900-01-01');
+  let checkedDate = false;
+  if (startDate.getTime() <= birthDate.getTime() && birthDate.getTime() < currentDate.getTime()) {
+    checkedDate = date;
+  };
+  return checkedDate;
+};
+
+function validateYear(year) {
+  const startYear = 2000;
+  let checkedYear = false;
+  if (startYear <= year && year <= currentYear) {
+    checkedYear = year;
+  };
+  return checkedYear;
+};
+
+function validateEndYear(year) {
+  const startYear = 2000;
+  let checkedYear = false;
+  if (startYear <= year && year <= (currentYear + STUDY_PERIOD)) {
+    checkedYear = year;
+  };
+  return checkedYear;
+};
 
 //валидация
 function validateName() {
@@ -271,37 +333,8 @@ function validateStartEducationYear() {
   }
 };
 
-//функции валидации input
-function validateText(text) {
-  let checkedText = false;
-  const regexp = /[а-яА-ЯёЁ]/i;
-  if (text.length > 0 && regexp.test(text)) {
-    checkedText = text[0].toUpperCase() + text.slice(1).toLowerCase();
-  };
-  return checkedText;
-};
-
-function validateDate(date) {
-  const birthDate = new Date(date);
-  const startDate = new Date('1900-01-01');
-  let checkedDate = false;
-  if (startDate.getTime() <= birthDate.getTime() && birthDate.getTime() < currentDate.getTime()) {
-    checkedDate = date;
-  };
-  return checkedDate;
-};
-
-function validateYear(year) {
-  const startYear = 2000;
-  let checkedYear = false;
-  if (startYear <= year && year < currentYear) {
-    checkedYear = year;
-  };
-  return checkedYear;
-};
-
 //очистка поля с ошибками
-function cleanErrorField() {
+function cleanErrorField(errorField) {
   if (!errorField) {
     return;
   }
@@ -332,7 +365,7 @@ function newId(arr) {
 
 formAdding.addEventListener('submit', function (e) {
   e.preventDefault();
-  cleanErrorField();
+  cleanErrorField(errorField);
   validateName();
   validateSurname();
   validatePatronymic();
@@ -355,7 +388,236 @@ formAdding.addEventListener('submit', function (e) {
 });
 
 // Этап 5. Создайте функцию сортировки массива студентов и добавьте события кликов на соответствующие колонки.
+const buttonFullname = document.querySelector('#Fullname');
+const buttonFaculty = document.querySelector('#Faculty');
+const buttonDateAge = document.querySelector('#DateAge');
+const buttonStudyYears = document.querySelector('#StudyYears');
 
+function arraySort(key, array, dir = false) {
+  let sortedArray = [...array];
+  sortedArray = sortedArray.sort(function (a, b) {
+    let sortDirection = dir ? (a[key] > b[key]) : (a[key] < b[key]);
+    if (sortDirection) return -1;
+  })
+
+  return sortedArray;
+}
+
+function arraySort(array, dir, ...keys) {
+  let sortedArray = [...array];
+  for (const key of keys) {
+    sortedArray = sortedArray.sort(function (a, b) {
+      let sortDirection = dir ? (a[key] > b[key]) : (a[key] < b[key]);
+      if (sortDirection) {
+        return -1;
+      }
+      if (!sortDirection) {
+        return 1;
+      }
+      return 0;
+    }
+    )
+  }
+  return sortedArray;
+}
+console.log('studentsList', studentsList);
+
+let buttonFullnameClicker = 0;
+buttonFullname.addEventListener('click', function () {
+  let dir = false;
+  if (buttonFullnameClicker) {
+    console.log('buttonFullnameClicker', buttonFullnameClicker);
+    dir = true;
+    buttonFullnameClicker = 0;
+  } else if (!buttonFullnameClicker) {
+    console.log('!buttonFullnameClicker', buttonFullnameClicker);
+    dir = false;
+    buttonFullnameClicker = 1;
+  };
+
+  let sortedArray = arraySort(studentsList, dir, 'patronymic', 'name', 'surname');
+
+  console.log('addFullNameArray', sortedArray);
+  renderStudentsTable(sortedArray);
+})
+
+let buttonFacultyClicker = 0;
+buttonFaculty.addEventListener('click', function () {
+  let dir = false;
+  if (buttonFacultyClicker) {
+    console.log('buttonFacultyClicker', buttonFacultyClicker);
+    dir = true;
+    buttonFacultyClicker = 0;
+  } else if (!buttonFacultyClicker) {
+    console.log('!buttonFacultyClicker', buttonFacultyClicker);
+    dir = false;
+    buttonFacultyClicker = 1;
+  };
+
+  let sortedArray = arraySort(studentsList, dir, 'faculty');
+
+  console.log('addFacultyArray', sortedArray);
+  renderStudentsTable(sortedArray);
+})
+
+let buttonDateAgeClicker = 0;
+buttonDateAge.addEventListener('click', function () {
+  let dir = false;
+  if (buttonDateAgeClicker) {
+    console.log('buttonDateAgeClicker', buttonDateAgeClicker);
+    dir = true;
+    buttonDateAgeClicker = 0;
+  } else if (!buttonDateAgeClicker) {
+    console.log('!buttonDateAgeClicker', buttonDateAgeClicker);
+    dir = false;
+    buttonDateAgeClicker = 1;
+  };
+
+  let sortedArray = arraySort(studentsList, dir, 'birthDate');
+
+  console.log('addBirthDateArray', sortedArray);
+  renderStudentsTable(sortedArray);
+})
+
+let buttonStudyYearsClicker = 0;
+buttonStudyYears.addEventListener('click', function () {
+  let dir = false;
+  if (buttonStudyYearsClicker) {
+    console.log('buttonStudyYearsClicker', buttonStudyYearsClicker);
+    dir = true;
+    buttonStudyYearsClicker = 0;
+  } else if (!buttonStudyYearsClicker) {
+    console.log('!buttonStudyYearsClicker', buttonStudyYearsClicker);
+    dir = false;
+    buttonStudyYearsClicker = 1;
+  };
+
+  let sortedArray = arraySort(studentsList, dir, 'startEducationYear');
+
+  console.log('addStudyYearsArray', sortedArray);
+  renderStudentsTable(sortedArray);
+})
 
 
 // Этап 6. Создайте функцию фильтрации массива студентов и добавьте события для элементов формы.
+const inputFilterFullName = document.querySelector('#input-filterFullName');
+const inputFilterFaculty = document.querySelector('#input-filterFaculty');
+const inputFilterStartEducationYear = document.querySelector('#input-filterStartEducationYear');
+const inputFilterEndEducationYear = document.querySelector('#input-filterEndEducationYear');
+const errorFieldFilter = document.querySelector('#errorFieldFilter');
+const buttonFilter = document.querySelector('#filterBtn');
+const buttonDeclineFilter = document.querySelector('#declineFilterBtn');
+
+function filterText(arr, value) {
+  let copy = [...arr];
+  let filteredArray = [];
+  for (let i = 0; i < copy.length; i += 1) {
+    for (key in copy[i]) {
+      if (copy[i][key] == value) filteredArray.push(copy[i]);
+    }
+  }
+  return filteredArray;
+}
+
+function filterFullName(arr) {
+  let valueFilterFullName = getInputValue(inputFilterFullName);
+  let arrayFilterFullName = valueFilterFullName.split(' ');
+
+  let arrayCheckedText = [];
+  for (const text of arrayFilterFullName) {
+    arrayCheckedText.push(validateText(text));
+  };
+
+  let filteredArray = [];
+  let array1 = [];
+  let array2 = [];
+  if (arrayCheckedText.length = 1) {
+    array1 = filterText(arr, arrayCheckedText[0]);
+    return array1;
+  };
+  if (arrayCheckedText.length = 2) {
+    array2 = filterText(array1, arrayCheckedText[1]);
+    return array2;
+  };
+  if (arrayCheckedText.length == 3) {
+    filteredArray = filterText(array2, arrayCheckedText[2]);
+    return filteredArray;
+  };
+
+};
+
+function filterFaculty(arr) {
+  let valueFilterFaculty = getInputValue(inputFilterFaculty);
+  let checkedFaculty = validateText(valueFilterFaculty);
+
+  console.log('checkedFaculty', checkedFaculty);
+
+  let filteredArray = [];
+  filteredArray = filterText(arr, checkedFaculty);
+  console.log('filterFaculty', filteredArray);
+  return filteredArray;
+};
+
+function filterStartEducationYear(arr) {
+  let valueFilterStartEducationYear = getInputValue(inputFilterStartEducationYear);
+  let checkedStartEducationYear = validateYear(valueFilterStartEducationYear);
+
+  let filteredArray = [];
+  filteredArray = filterText(arr, checkedStartEducationYear);
+  return filteredArray;
+};
+
+function filterEndEducationYear(arr) {
+  let valueFilterEndEducationYear = getInputValue(inputFilterEndEducationYear);
+  valueFilterEndEducationYear = validateEndYear(valueFilterEndEducationYear);
+  checkedStartEducationYear = valueFilterEndEducationYear - STUDY_PERIOD;
+
+  let filteredArray = [];
+  filteredArray = filterText(arr, checkedStartEducationYear);
+  return filteredArray;
+};
+
+
+buttonFilter.addEventListener('click', function (e) {
+  e.preventDefault();
+  let checkedFullName = validateText(getInputValue(inputFilterFullName));
+  let checkedFaculty = validateText(getInputValue(inputFilterFaculty));
+  let checkedStartEducationYear = validateYear(getInputValue(inputFilterStartEducationYear));
+  let checkedEndEducationYear = validateEndYear(getInputValue(inputFilterEndEducationYear));
+
+  cleanErrorField(errorFieldFilter);
+
+  if (!checkedFullName && !checkedFaculty && !checkedStartEducationYear && !checkedEndEducationYear) {
+    let alert = document.createElement('p');
+    alert.classList.add('mark');
+    alert.textContent = 'Поля не заполнены или заполнены некорректно';
+    errorFieldFilter.append(alert);
+    return;
+  };
+
+  let filteredArray = [...studentsList];
+  if (checkedFullName) {
+    filteredArray = filterFullName(filteredArray);
+  }
+  if (checkedFaculty) {
+    filteredArray = filterFaculty(filteredArray);
+  }
+  if (checkedStartEducationYear) {
+    filteredArray = filterStartEducationYear(filteredArray);
+  }
+  if (checkedEndEducationYear) {
+    filteredArray = filterEndEducationYear(filteredArray);
+  }
+  renderStudentsTable(filteredArray);
+
+  clearInputValue(inputFilterFullName);
+  clearInputValue(inputFilterFaculty);
+  clearInputValue(inputFilterStartEducationYear);
+  clearInputValue(inputFilterEndEducationYear);
+});
+
+buttonDeclineFilter.addEventListener('click', function (e) {
+  e.preventDefault();
+  cleanErrorField(errorFieldFilter);
+  renderStudentsTable(studentsList);
+});
